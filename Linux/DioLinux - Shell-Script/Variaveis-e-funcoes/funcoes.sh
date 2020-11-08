2f648f7de00dc7282ea43749324fb38a786791a2
Keviny Joubert <61211057+kevinyjoubert@users.noreply.github.com>
2020-11-08 13:20:21 -0300

    funções add

---
+++ Linux/DioLinux - Shell-Script/Variaveis-e-funcoes/funcoes.sh
@@ -0,0 +1,9 @@
+#definindo funcoes no shell
+
+#digite o nome da funcao e coloque parenteses e passe os argumentos nas chaves
+
+#exemplo listando todos os arquivos e os ocultos
+
+lla () {ls -la}
+
+export lla
