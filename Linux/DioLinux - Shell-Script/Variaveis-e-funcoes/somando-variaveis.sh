421d1669963872d9f28fb745b49440f10862c61e
Keviny Joubert <61211057+kevinyjoubert@users.noreply.github.com>
2020-11-08 13:12:17 -0300

    somando variavels

---
+++ Linux/DioLinux - Shell-Script/Variaveis-e-funcoes/somando-variaveis.sh
@@ -0,0 +1,16 @@
+#SOMANDO VARIAVEIS
+
+NUMERO_1=2
+
+NUMERO_2=2
+
+echo $(($NUMERO_1+$NUMERO_2))
+
+#ou colocando em outra variavel e chamando ela
+
+echo
+echo
+
+TOTAL=$(($NUMERO_1+$NUMERO_2))
+
+echo $TOTAL
