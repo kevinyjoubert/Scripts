05c66253a155d8e7c921d492024e96a5bd014643
Keviny Joubert <61211057+kevinyjoubert@users.noreply.github.com>
2020-11-11 17:17:00 -0300

    teste de requisito 2

--- Linux/DioLinux - Shell-Script/Teste-de-requisito/teste-de-requisito.sh
+++ Linux/DioLinux - Shell-Script/Teste-de-requisito/teste-de-requisito.sh
@@ -15,3 +15,14 @@ if [[ ! -x `which wget` ]]
 
 #testa se tem conexao de internet
 
+if ! ping -c 1 8.8.8.8 -q &> /dev/null
+    then
+        echo
+        echo "[ERROR] - Seu computador está sem conexão com a internet, verifique!"
+        echo
+        exit 1
+    else
+        echo
+        echo "[INFO] - Conexao com a internet funcionando"
+        echo
+fi
