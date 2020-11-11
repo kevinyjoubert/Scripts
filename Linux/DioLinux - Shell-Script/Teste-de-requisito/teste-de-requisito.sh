d10cfde8d04264dd6351b095c838e6b50b7c879a
Keviny Joubert <61211057+kevinyjoubert@users.noreply.github.com>
2020-11-11 15:47:35 -0300

    teste de requisito 1

--- Linux/DioLinux - Shell-Script/Teste-de-requisito/teste-de-requisito.sh
+++ Linux/DioLinux - Shell-Script/Teste-de-requisito/teste-de-requisito.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+#teste de requisito, serve para validar se algum arquivo ou programa existe
+
+
+# testa se o pacote wget esta instalado
+
+if [[ ! -x `which wget` ]]
+    then
+        echo "[INFO] wget não está instalado, o mesmo será instalado!"
+        sudo apt install wget -y
+    else
+        echo "[INFO] o wget já está instalado!"
+    fi
+
+#testa se tem conexao de internet
+
