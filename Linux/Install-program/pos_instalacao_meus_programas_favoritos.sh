d14065c4d4ea2eedc6f961b4b652e5d85893a4ab
Keviny Joubert <61211057+kevinyjoubert@users.noreply.github.com>
2020-11-12 00:27:24 -0300

    iniciando script para instalação de programas automatico

---
+++ Linux/Install-program/pos_instalacao_meus_programas_favoritos.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# pos_instalacao_meus_programas_favoritos.sh - Faz a instalação dos programas 
+# que costumo utilizar no Linux
+#
+# Github:       https://github.com/kevinyjoubert
+# Linkedin:     https://www.linkedin.com/in/keviny-joubert-2992b8b1/
+# Autor:        Keviny Joubert
+#
+# -------------------------------------------------------------------------------
+#
+# -O QUE O SCRIPT FAZ?
+#
+#   Esse script instala os programas que utilizo após a formatação, faz upgrade
+#  e limpeza do sistema. 
+#
+#
+# -COMO USAR?
+#
+#   Abra o terminal aonde se encontra o arquivo e execute o comando abaixo:
+#
+#      $  ./pos_instalação_meus_programas_favoritos.sh
+#
+#
+# -NOTA:
+#
+#   O Script foi pensado para ter uma boa organização, fácil manutenção e para
+#  quem queira editar ao seu uso!
+#
+# -------------------------------------------------------------------------------
+#
+# -Changelog:
+#
+#   v1.0 DATA, Keviny Joubert:
+#       - Primeira versão com poucos programas
+#
+# -------------------------------------------------------------------------------
