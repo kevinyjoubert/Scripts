6616de7462c355ddc885f22cdffe36cfa9c64e08
Keviny Joubert <61211057+kevinyjoubert@users.noreply.github.com>
2020-11-13 15:34:44 -0300

    iniciando script de backup windows automatico

---
+++ Windows/Backup-automatico/backup-automatico.bat
@@ -0,0 +1,41 @@
+REM!/bin/bash
+REM
+REM pos_instalação_meus_programas_favoritos.sh - Faz a instalação dos programas 
+REM que costumo utilizar no Linux
+REM
+REM Github:       https://github.com/kevinyjoubert
+REM Linkedin:     https://www.linkedin.com/in/keviny-joubert-2992b8b1/
+REM Autor:        Keviny Joubert
+REM
+REM -------------------------------------------------------------------------------
+REM
+REM -O QUE O SCRIPT FAZ?
+REM
+REM   Esse script instala os programas que utilizo após a formatação, faz upgrade
+REM  e limpeza do sistema. 
+REM
+REM
+REM -COMO USAR?
+REM
+REM   Abra o terminal aonde se encontra o arquivo e execute o comando abaixo:
+REM
+REM      $  ./pos_instalação_meus_programas_favoritos.sh
+REM
+REM
+REM -NOTA:
+REM
+REM   O Script foi pensado para ter uma boa organização, fácil manutenção e para
+REM  quem queira editar ao seu uso!
+REM
+REM -------------------------------------------------------------------------------
+REM
+REM -Changelog:
+REM
+REM   v1.0 DATA, Keviny Joubert:
+REM       - Primeira versão com poucos programas
+REM
+REM -------------------------------------------------------------------------------
+
+
+set Origem=C:\Users\Public\Wallpaper
+set Destino=X:\Backup\teste-backup
