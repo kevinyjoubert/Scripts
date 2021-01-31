bfcd241e05958ac16c49e02d3027025f818e0a3f
Keviny Joubert <kevinytaka@gmail.com>
2021-01-31 20:17:44 -0300

    description of archives

--- Windows/block-users/block-users.bat
+++ Windows/block-users/block-users.bat
@@ -1,8 +1,8 @@
 @echo off
 @REM
-@REM - Nome: backup-automatico.bat
+@REM - Nome: block-users.bat
 @REM
-@REM - Objetivo: Realizar backup mais facilmente da pasta do usuário, todos usuários ou de arquivos de extensões isoladas
+@REM - Objetivo: Script feito para bloquear usuários no computador.
 @REM
 @REM Github:       https://github.com/kevinyjoubert
 @REM Linkedin:     https://www.linkedin.com/in/keviny-joubert-2992b8b1/
@@ -12,24 +12,28 @@
 @REM
 @REM -O que o Script faz?
 @REM
-@REM    Esse Script foi feito para realizar backup mais facilmente, dando opções de backup da pasta do usuário, todos usuários, extensões de musicas, fotos, videos e remoto.
+@REM    Esse Script foi feito para realizar o bloqueio de usuarios em uma rede AD de empresas,
+@REM    porém pode ser usado para outras finalidades.
 @REM
 @REM
 @REM -Como usar?
 @REM
-@REM    Abra o Script com duplo clique e utilize, caso queira mudar alguma pasta origem ou backup, computador remoto ou log-backup, altere antes de executar o Script.
+@REM    Abra o script no modo editavel e coloque os usuários que você deseja dar permissão e
+@REM    logo em seguida, abra o script cp.bat para ser copiado para todos os usuários
 @REM
 @REM
 @REM -Nota:
 @REM
-@REM    O Script foi pensado para facilitar o processo de backup tanto para formatação ou facilidade do usuário
+@REM    O Script foi pensado para impedir que usuários não permitidos acessem um computador
+@REM    em especifico, lembrando que a cada usuário novo, o script deve ter o usuário 
+@REM    permitido e executar o arquivo cp.bat novamente
 @REM
 @REM -----------------------------------------------------------------------------------------
 @REM
 @REM -Changelog:
 @REM
-@REM    v1.0 - 10/01/2021, Keviny Joubert:
-@REM       - Backup usuário, todos usuários, imagens, videos, musica, remoto
+@REM    v1.0 - 30/01/2021, Keviny Joubert:
+@REM       - Bloqueio de usuário por matricula
 @REM
 @REM -----------------------------------------------------------------------------------------
 
--- Windows/block-users/cp.bat
+++ Windows/block-users/cp.bat
@@ -1,4 +1,39 @@
-
+@echo off
+@REM
+@REM - Nome: cp.bat
+@REM
+@REM - Objetivo: Script feito para copiar o script block-users.bat para todos os usuários;
+@REM
+@REM Github:       https://github.com/kevinyjoubert
+@REM Linkedin:     https://www.linkedin.com/in/keviny-joubert-2992b8b1/
+@REM Autor:        Keviny Joubert
+@REM
+@REM -----------------------------------------------------------------------------------------
+@REM
+@REM -O que o Script faz?
+@REM
+@REM    Esse Script foi feito para copiar automáticamente para todos os usuarios o script
+@REM    block-users.bat
+@REM
+@REM
+@REM -Como usar?
+@REM
+@REM    Abra o computador no usuário Administrador e coloque os arquivos no Desktop,
+@REM    block-users.bat e o cp.bat e execute o arquivo cp.bat
+@REM
+@REM
+@REM -Nota:
+@REM
+@REM    O Script foi pensado pra agilizar a cópia do arquivo para todos os usuários
+@REM
+@REM -----------------------------------------------------------------------------------------
+@REM
+@REM -Changelog:
+@REM
+@REM    v1.0 - 30/01/2021, Keviny Joubert:
+@REM       - Copiar o arquivo block-users.bat para todos usuários
+@REM
+@REM -----------------------------------------------------------------------------------------
 
 @echo off
 
